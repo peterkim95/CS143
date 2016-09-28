c833ca6b041168a52d09f709666ba0107eab44d4
Peter Kim <peterkim95@ucla.edu>
2016-09-28 14:17:07 -0700

    made it super simple now

--- 1a/calculator.php
+++ 1a/calculator.php
@@ -14,16 +14,12 @@
 
     if(isset($_GET['expr'])) {
       $expr = str_replace(' ', '', $_GET['expr']);
-      $valid_chars_only = preg_match("/^[-+*.\/, 0-9]+$/", $expr);
-      if ($valid_chars_only) {
-        $err = @eval("\$ans=$expr;");
-        if (error_get_last()) {
-          echo "Error!";
-        } else {
-          echo $expr . " = " . $ans;
-        }
+
+      $err = @eval("\$ans=$expr;");
+      if (error_get_last()) {
+        echo "Error for whatever reason...distinguish them later? Spec doesn't mention";
       } else {
-        echo "Error! no alphabets pls";
+        echo $expr . " = " . $ans;
       }
     }
 
