46a30589e066fe5dcbc7990453cd0f0296edd1a3
Peter Kim <peterkim95@ucla.edu>
2016-10-04 17:13:07 -0700

    audio medic

--- 1a/calculator.php
+++ 1a/calculator.php
@@ -4,7 +4,8 @@
     <title>PHP Calculator</title>
   </head>
   <body>
-
+    <h1>Calculator</h1>
+    <div>By Peter Kim & Scott Shi</div>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
       <input type="text" name="expr">
       <input type="submit" value="Calculate">
@@ -13,14 +14,37 @@
     <?php
 
     if(isset($_GET['expr'])) {
-      $expr = str_replace(' ', '', $_GET['expr']);
+      $expr = str_replace(' ', '', $_GET['expr']);  // eliminate spaces
 
-      $err = @eval("\$ans=$expr;");
-      if (error_get_last()) {
-        echo "Error for whatever reason...distinguish them later? Spec doesn't mention";
+      $valid_char = preg_match("/^[-+*.\/, 0-9]+$/", $expr);
+      $valid_num_beginning = preg_match("/^[0]+[1-9]/", $expr);
+      $valid_num = preg_match("/[^1-90][0]+[1-9]/", $expr);
+      $decimal_beginning = preg_match("/^\./", $expr);
+      $decimal = preg_match("/[^1-90]\./", $expr);
+      $div_zero = preg_match("/\/[0]/", $expr);
+
+      $expr = str_replace('--','- -',$expr);
+
+      // echo $expr;
+      if (empty($expr)) {
+        // output nothing
+      } else if (!$valid_char || $valid_num_beginning || $valid_num || $decimal_beginning || $decimal) {
+        echo "<h1>Result</h1>";
+        echo "Invalid Expression!";
+      } else if ($div_zero) {
+        echo "<h1>Result</h1>";
+        echo "Division by zero error!";
       } else {
-        echo $expr . " = " . $ans;
+        echo "<h1>Result</h1>";
+        $err = @eval("\$ans=$expr;");
+        if (error_get_last()) {
+          echo "Invalid Expression!";
+        } else {
+          echo $expr . " = " . $ans;
+        }
       }
+
+
     }
 
     ?>
---
+++ 1a/readme.txt
@@ -0,0 +1,1 @@
+Scott figured out edge cases and appropriate/expected regex expression that could capture them. Peter implemented the regex and tested them out and fixed any issues.
